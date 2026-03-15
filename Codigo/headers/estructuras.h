d6ae612a06a621503580a4ee5379c69bbc89b18a
MaikelFn <maikelflores27@gmail.com>
2026-03-15 13:04:22 -0600

    Headers y estructura inicial del proyecto

---
+++ Codigo/headers/estructuras.h
@@ -0,0 +1,22 @@
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
+
+typedef struct {
+    char detalle[100];
+    char Ubicacion[100];
+    char sitioweb[100];
+} SitioEvento;
+
+typedef struct {
+} Sector;
+
+typedef struct {
+} Asiento;
+
+typedef struct {
+} Evento;
+
+typedef struct {
+} Factura;
+
+#endif
